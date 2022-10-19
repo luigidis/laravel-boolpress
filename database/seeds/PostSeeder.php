7d68ef58b96430dfb8a8feba5fcbdc33dac3c088
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-19 16:00:06 +0200

    seed launched (working)

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 50; $i++) { 
+            
+            $post = new Post();
+            $post->title = $faker->words( rand(5,10), true);
+            $post->content = $faker->paragraphs( rand(10,20), true);
+            $post->slug = str_replace(' ', '-', $post->title);
+
+            $post->save();
+        }
     }
 }
